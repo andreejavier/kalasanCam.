1e7a37203cdb203880d21aad7122470737da71a5
andreejavier <20211241@nbsc.edu.ph>
2024-07-30 21:48:30 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'kalasan-tree-mapping',
+  webDir: 'dist'
+};
+
+export default config;
